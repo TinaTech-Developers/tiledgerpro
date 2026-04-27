97a66149747072ddf9178fafc9c3cc544549e8a6
TinaTech-Developers <tinashephiri0@gmail.com>
2026-04-27 16:50:19 +0200

    added nodejs test

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,4 +1,5 @@
-import { prisma } from "../../../lib/prisma";
+export const runtime = "nodejs";
+import { prisma } from "@/lib/prisma";
 
 export async function GET() {
   const org = await prisma.organization.create({
