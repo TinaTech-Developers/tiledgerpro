84560189c185d25997615f691b8667956ba319aa
TinaTech-Developers <tinashephiri0@gmail.com>
2026-05-05 12:30:10 +0200

    production urld

--- lib/api.ts
+++ lib/api.ts
@@ -1,23 +1,18 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "";
-
 export const apiFetch = async (url: string, options: any = {}) => {
-  const token =
-    typeof window !== "undefined" ? localStorage.getItem("token") : null;
+  const token = localStorage.getItem("token");
 
-  const res = await fetch(`${BASE_URL}${url}`, {
+  const res = await fetch(url, {
     ...options,
     headers: {
       "Content-Type": "application/json",
-      Authorization: token ? `Bearer ${token}` : "",
+      Authorization: `Bearer ${token}`,
       ...(options.headers || {}),
     },
   });
 
   if (res.status === 401) {
-    if (typeof window !== "undefined") {
-      alert("Session expired. Please login again.");
-      window.location.href = "/login";
-    }
+    alert("Session expired. Please login again.");
+    window.location.href = "/login";
     return;
   }
 
